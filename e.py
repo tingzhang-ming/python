be6ae90221b709a763a4f3115edc32c07c83f4cd
mhcvs2 <merzhong0501@163.com>
2018-01-21 20:59:21 +0800

    Merge branch 'master' of https://github.com/mhcvs2/python
    Conflicts:
    	e.py

--- e.py
+++ e.py
@@ -1,18 +1,1 @@
-import time
 
-
-def t1():
-    a = 'adfdf'
-    print a.title()
-
-
-def t2():
-    print time.time()
-
-
-def t3():
-    print 0 * 100 / 0
-
-
-if __name__ == '__main__':
-    t3()
