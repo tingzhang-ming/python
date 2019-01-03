300c4fb6c00532cfa7280d3c4a9efdbf5d6e57ad
MAHONGCHAO [马洪超] <MAHONGCHAO@kingsoft.cn>
2019-01-03 20:39:42 +0800

    add

--- shell/git.py
+++ shell/git.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import tempfile
+import shutil
 import subprocess
 
 github_user = "mhcvs2"
@@ -57,11 +58,11 @@ def git_auth():
     run_shell('git config --global credential.helper store')
     with open(git_auth_file, 'w') as f:
         lines = []
-        if gitee_user:
+        if gitee_user and gitee_password:
             lines.append('https://{}:{}@gitee.com\n'.format(gitee_user, gitee_password))
-        if github_user:
+        if github_user and github_password:
             lines.append('https://{}:{}@github.com\n'.format(github_user, github_password))
-        if gitlab_user:
+        if gitlab_user and gitlab_user:
             lines.append('http://{}:{}@gitlab.gcloud.srcb.com\n'.format(gitlab_user, gitlab_password))
         f.writelines(lines)
 
@@ -70,25 +71,33 @@ def clear(work_dir):
     # if os.path.exists(git_auth_file):
     #     os.remove(git_auth_file)
     if work_dir and os.path.isdir(work_dir):
-        os.rmdir(work_dir)
+        shutil.rmtree(work_dir)
 
 
 def main():
-    work_dir = None
-    try:
-        work_dir = tempfile.mkdtemp()
-        print "work dir: ", work_dir
-        os.chdir(work_dir)
-        git_auth()
-        for pc in pull_config:
-            for p in pc['projects']:
-                clone_and_rmgit(pc['url'], pc['repo'], p)
-        push(push_config['url'], push_config['repo'], push_config['project'])
-    except Exception as e:
-        print "error: "
-        print e.message
-    finally:
-        clear(work_dir)
+    work_dir = tempfile.mkdtemp()
+    print "work dir: ", work_dir
+    os.chdir(work_dir)
+    git_auth()
+    for pc in pull_config:
+        for p in pc['projects']:
+            clone_and_rmgit(pc['url'], pc['repo'], p)
+    push(push_config['url'], push_config['repo'], push_config['project'])
+    # work_dir = None
+    # try:
+    #     work_dir = tempfile.mkdtemp()
+    #     print "work dir: ", work_dir
+    #     os.chdir(work_dir)
+    #     git_auth()
+    #     for pc in pull_config:
+    #         for p in pc['projects']:
+    #             clone_and_rmgit(pc['url'], pc['repo'], p)
+    #     push(push_config['url'], push_config['repo'], push_config['project'])
+    # except Exception as e:
+    #     print "error: "
+    #     print e.message
+    # finally:
+    #     clear(work_dir)
 
 
 if __name__ == '__main__':
